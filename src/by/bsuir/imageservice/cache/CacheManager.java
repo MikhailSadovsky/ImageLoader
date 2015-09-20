f24e67262f8b8a743d90620bb5bceae26023549b
MikhailSadovsky <mishachess@mail.ru>
2015-09-20 20:14:22 +0300

    Add gui field and call it's methods

--- src/by/bsuir/imageservice/cache/CacheManager.java
+++ src/by/bsuir/imageservice/cache/CacheManager.java
@@ -1,6 +1,7 @@
 package by.bsuir.imageservice.cache;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -8,91 +9,106 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.log4j.Logger;
 
+import by.bsuir.imageservice.GUI;
 import by.bsuir.imageservice.exception.ProjectException;
 
 /**
  * CacheManager simplify operations for holding cacheable objects
  * 
  * @author Mikhail_Sadouski
- *
+ * 
  */
 public class CacheManager {
-	private static final Logger log = Logger.getLogger(CacheManager.class);
+    private static final Logger log = Logger.getLogger(CacheManager.class);
 
-	/**
-	 * Sleep time for cleaner thread
-	 */
-	private static final int DEFAULT_MILLISECOND_SLEEP_TIME = 5000;
-	private static Map<Object, Cacheable> cacheHashMap = new ConcurrentHashMap<Object, Cacheable>();
+    /**
+     * Sleep time for cleaner thread
+     */
+    private static final int DEFAULT_MILLISECOND_SLEEP_TIME = 5000;
+    private static Map<Object, Cacheable> cacheHashMap = new ConcurrentHashMap<Object, Cacheable>();
+    private static GUI gui;
 
-	/**
-	 * Initialize cleaner thread, that checks objects for expiring and remove
-	 */
-	static {
-		try {
-			Thread cleaner = new Thread(new Runnable() {
+    /**
+     * Initialize cleaner thread, that checks objects for expiring and remove
+     */
+    static {
+	try {
+	    Thread cleaner = new Thread(new Runnable() {
 
-				public void run() {
-					try {
-						while (true) {
-							Set<Object> keySet = cacheHashMap.keySet();
-							Iterator<Object> keys = keySet.iterator();
-							while (keys.hasNext()) {
-								Object key = keys.next();
-								Cacheable value = (Cacheable) cacheHashMap
-										.get(key);
-								if (value.isExpired()) {
-									File outputFile = new File(key.toString());
-									if (outputFile.delete()) {
-										cacheHashMap.remove(key);
-										log.info("Item " + key
-												+ " was deleted ");
-									} else {
-										log.info("Item " + key
-												+ " fails to delete ");
-									}
-								}
-							}
-							Thread.sleep(DEFAULT_MILLISECOND_SLEEP_TIME);
-						}
-					} catch (InterruptedException e) {
-						log.error(e.getMessage(), e);
-					}
-					return;
+		public void run() {
+		    try {
+			while (true) {
+			    Set<Object> keySet = cacheHashMap.keySet();
+			    Iterator<Object> keys = keySet.iterator();
+			    while (keys.hasNext()) {
+				Object key = keys.next();
+				Cacheable value = (Cacheable) cacheHashMap
+					.get(key);
+				if (value.isExpired()) {
+				    File outputFile = new File(key.toString());
+				    if (outputFile.delete()) {
+					cacheHashMap.remove(key);
+					gui.updateImageTable(getCacheMap());
+					log.info("Item " + key
+						+ " was deleted ");
+				    } else {
+					log.info("Item " + key
+						+ " fails to delete ");
+				    }
 				}
-			});
-			cleaner.setPriority(Thread.MIN_PRIORITY);
-			cleaner.start();
-		} catch (Exception e) {
+			    }
+			    Thread.sleep(DEFAULT_MILLISECOND_SLEEP_TIME);
+			}
+		    } catch (InterruptedException e) {
 			log.error(e.getMessage(), e);
-			throw new ProjectException(e.getMessage(), e);
+		    }
+		    return;
 		}
-	}
 
-	/**
-	 * Put object in cache
-	 * 
-	 * @param object
-	 */
-	public static void putObject(Cacheable object) {
-		cacheHashMap.put(object.getIdentifier(), object);
+	    });
+	    cleaner.setPriority(Thread.MIN_PRIORITY);
+	    cleaner.start();
+	} catch (Exception e) {
+	    log.error(e.getMessage(), e);
+	    throw new ProjectException(e.getMessage(), e);
 	}
+    }
 
-	/**
-	 * Get object from cache by it's identifier or return null if nothing find
-	 * 
-	 * @param identifier
-	 * @return cacheable object
-	 */
-	public static Cacheable getObject(Object identifier) {
-		Cacheable object = (Cacheable) cacheHashMap.get(identifier);
-		if (null == object)
-			return null;
-		if (object.isExpired()) {
-			cacheHashMap.remove(identifier);
-			return null;
-		} else {
-			return object;
-		}
+    /**
+     * Put object in cache
+     * 
+     * @param object
+     */
+    public static void putObject(Cacheable object) {
+	cacheHashMap.put(object.getIdentifier(), object);
+	gui.updateImageTable(getCacheMap());
+    }
+
+    /**
+     * Get object from cache by it's identifier or return null if nothing find
+     * 
+     * @param identifier
+     * @return cacheable object
+     */
+    public static Cacheable getObject(Object identifier) {
+	Cacheable object = (Cacheable) cacheHashMap.get(identifier);
+	if (null == object)
+	    return null;
+	if (object.isExpired()) {
+	    cacheHashMap.remove(identifier);
+	    return null;
+	} else {
+	    return object;
 	}
+    }
+
+    public static void setGUI(GUI gui) {
+	CacheManager.gui = gui;
+    }
+
+    public static Map<Object, Cacheable> getCacheMap() {
+	Map<Object, Cacheable> shallowCopy = new HashMap<Object, Cacheable>();
+	shallowCopy.putAll(cacheHashMap);
+	return shallowCopy;
+    }
 }
